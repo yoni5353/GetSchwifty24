8730c30487c1d2b169ce2c83c4427eca0d4a73fe
Yonatan Skourjinsky <yoni.sku@gmail.com>
2022-07-03 17:38:25 +0300

    Changed js block indentation to two spaces.

--- bootstrapper.js
+++ bootstrapper.js
@@ -2,8 +2,8 @@ import { Board } from "./model/board.js";
 import { updateBoard } from "./view/gamedisplay.js";
 
 function main() {
-    let gameboard = new Board(3, 3);
-    updateBoard(gameboard.board);
+  let gameboard = new Board(3, 3);
+  updateBoard(gameboard.board);
 }
 
 main();
--- model/board.js
+++ model/board.js
@@ -1,9 +1,9 @@
 export class Board {
-    constructor(width, height) {
-        this.board = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 0],
-        ]
-    }
+  constructor(width, height) {
+    this.board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 0],
+    ]
+  }
 }
