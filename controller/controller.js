eb7fc6c139587906dceb84688f0b9a5c36d0f87c
Yonatan Skourjinsky <yoni.sku@gmail.com>
2022-07-04 10:15:01 +0300

    Advanced click functionality to detect two consecutive button presses.

--- controller/controller.js
+++ controller/controller.js
@@ -2,17 +2,25 @@ export class SchwiftyController {
   constructor(view, board) {
     this.gamedisplay = view;
     this.gameboard = board;
+    this.lastPressed = undefined;
   }
-
+  
   startGame() {
     this.gamedisplay.createBoard(this.gameboard.board);
 
     for (let button of this.gamedisplay.buttons) {
-      button.addEventListener("click", () => clicked(button.index))
+      button.addEventListener("click", () => this.clicked(button.index))
     }
   }
-}
 
-function clicked(buttonIndex) {
-  console.log(`Button ${buttonIndex} clicked!`)
+  clicked(buttonIndex) {
+    if (this.lastPressed === undefined) {
+      console.log(`Button ${buttonIndex} clicked!`)
+      this.lastPressed = buttonIndex;
+      return;
+    }
+    
+    console.log(`Buttons ${this.lastPressed} and ${buttonIndex} clicked!`)
+    this.lastPressed = undefined;
+  }
 }
